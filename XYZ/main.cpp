90853d93a45ee406176fb2651ea27b71a388981f
Tobin Anthony <tobin.anthony@spacint.com>
2015-12-25 20:26:20 -0500

    Initial Commit

---
+++ XYZ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XYZ
+//
+//  Created by tobin on 12/25/15.
+//  Copyright © 2015 tobin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
